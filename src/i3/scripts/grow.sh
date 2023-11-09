e4d5352696a8d97242a7a2b67e742afcf84ae695
Cody Hiar <cody@hiar.ca>
2023-11-09 09:48:30 -0700

    adding open_bookmarks with rofi script

--- src/i3/scripts/grow.sh
+++ src/i3/scripts/grow.sh
@@ -1,28 +1,41 @@
 #!/usr/bin/env bash
-# Script to grow i3 windows
+#
+# Author: Cody Hiar
+# Date: 2020-11-22
+#
+# Description: Script used to dynamically grow i3 windows.
+#
+# Set options:
+#   e: Stop script if command fails
+#   u: Stop script if unset variable is referenced
+#   x: Debug, print commands as they are executed
+#   o pipefail:  If any command in a pipeline fails it all fails
+#
+# IFS: Internal Field Separator
+set -uo pipefail
+IFS=$'\n\t'
 
 DIR=$1
 AMOUNT="30"
 
 if [[ "$DIR" == 'down' ]]; then
-    i3-msg "resize grow down ${AMOUNT}" &> /dev/null
-    if [[ "$?" == 2 ]]; then
-        i3-msg "resize shrink height ${AMOUNT}"
-    fi
+	i3-msg "resize grow down ${AMOUNT}" &>/dev/null
+	if [[ "$?" == 2 ]]; then
+		i3-msg "resize shrink height ${AMOUNT}"
+	fi
 elif [[ "$DIR" == "left" ]]; then
-    i3-msg "resize grow left ${AMOUNT}" &> /dev/null
-    if [[ "$?" == 2 ]]; then
-        i3-msg "resize shrink width ${AMOUNT}"
-    fi
+	i3-msg "resize grow left ${AMOUNT}" &>/dev/null
+	if [[ "$?" == 2 ]]; then
+		i3-msg "resize shrink width ${AMOUNT}"
+	fi
 elif [[ "$DIR" == "right" ]]; then
-    i3-msg "resize grow right ${AMOUNT}" &> /dev/null
-    if [[ "$?" == 2 ]]; then
-        i3-msg "resize shrink width ${AMOUNT}"
-    fi
+	i3-msg "resize grow right ${AMOUNT}" &>/dev/null
+	if [[ "$?" == 2 ]]; then
+		i3-msg "resize shrink width ${AMOUNT}"
+	fi
 elif [[ "$DIR" == "up" ]]; then
-    i3-msg "resize grow up ${AMOUNT}" &> /dev/null
-    if [[ "$?" == 2 ]]; then
-        i3-msg "resize shrink height ${AMOUNT}"
-    fi
+	i3-msg "resize grow up ${AMOUNT}" &>/dev/null
+	if [[ "$?" == 2 ]]; then
+		i3-msg "resize shrink height ${AMOUNT}"
+	fi
 fi
-
---
+++ src/i3/scripts/open_bookmarks.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+#
+# Author: Cody Hiar
+# Date: 2022-01-24
+#
+# Description: Use rofi to select bookmarks
+#
+# ~/.bookmarks.csv is just a csv that looks like:
+#
+# duck duck go,https://www.duckduckgo.com
+# my other site,https://example.com
+#
+# The first column being whatever I'd type to autocomplete and the second the
+# url.
+#
+# Set options:
+#   e: Stop script if command fails
+#   u: Stop script if unset variable is referenced
+#   x: Debug, print commands as they are executed
+#   o pipefail:  If any command in a pipeline fails it all fails
+#
+# IFS: Internal Field Separator
+set -uo pipefail
+IFS=$'\n\t'
+
+# Use cat to prefix everything with an index number. AFAIK there is not way to
+# get rofi to return the index of the selection you've chosen so I used this as
+# an alternative.
+items=$(cat -n ~/.bookmarks.csv)
+selection=$(echo "$items" | awk -F ',' '{print $1}' | rofi -dmenu -i -P Bookmark)
+# Make sure there was a selection and I didn't cancel out of the window
+if [[ -n "$selection" ]]; then
+	index=$(echo "$selection" | awk '{ print $1 }')
+	url=$(awk "NR==$index" ~/.bookmarks.csv | cut -d ',' -f 2)
+	xdg-open "$url"
+fi
