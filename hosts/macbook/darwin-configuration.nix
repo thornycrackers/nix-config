14f1a9725379f924b79437cd22a636a24482e53d
Cody Hiar <cody@hiar.ca>
2023-11-12 22:13:36 -0700

    Add tailscale to macbook

--- hosts/macbook/darwin-configuration.nix
+++ hosts/macbook/darwin-configuration.nix
@@ -66,6 +66,9 @@
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
 
+  # Setup tailscale private network
+  services.tailscale.enable = true;
+
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
--- hosts/shared/packages-base.nix
+++ hosts/shared/packages-base.nix
@@ -18,5 +18,6 @@ with pkgs; [
   lf
   neofetch
   nixos-rebuild
+  tailscale
   wget
 ]
