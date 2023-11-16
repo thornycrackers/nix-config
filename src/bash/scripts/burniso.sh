b3f21fc32b20af95f8fe3db9bd6c1c73171a3c92
Cody Hiar <cody@hiar.ca>
2023-11-16 13:44:36 -0700

    add burniso

---
+++ src/bash/scripts/burniso.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+# vim: set filetype=sh
+#
+# Author: Cody Hiar
+# Date: 2019-03-27
+#
+# Description: Burn an iso to a disk. I can never remember.
+#
+# Set options:
+#   e: Stop script if command fails
+#   u: Stop script if unset variable is referenced
+#   x: Debug, print commands as they are executed
+#   o pipefail:  If any command in a pipeline fails it all fails
+#
+# IFS: Internal Field Separator
+set -euo pipefail
+IFS=$'\n\t'
+
+ISO=$(find . -type f -name "*.iso" | fzf)
+DISK=$(lsblk -l | grep disk | fzf)
+
+echo "Selected disk is:"
+echo "$DISK"
+echo ""
+read -r -p "Are You Sure? [Y/n] " input
+
+DISK_PATH=$(echo "$DISK" | awk '{ print $1 }')
+
+case $input in
+[yY][eE][sS] | [yY])
+	echo 'run watch "cat /proc/meminfo | grep -i dirty" to watch progress'
+	sudo dd if="$ISO" of="/dev/$DISK_PATH" bs=4096 oflag=sync status=progress
+	;;
+[nN][oO] | [nN])
+	echo "You say No"
+	;;
+*)
+	echo "Invalid input..."
+	exit 1
+	;;
+esac
