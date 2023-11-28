3e0837813c448fa7e75477318700d6c5333b3cfc
Cody Hiar <cody@hiar.ca>
2023-11-28 09:52:25 -0700

    Breakout desktop packages into separate file

---
+++ hosts/shared/packages-linux-desktop.nix
@@ -0,0 +1,36 @@
+pkgs:
+with pkgs; let
+  # Note to future self, if you ran the native install curl script, you'll
+  # need to remove the items manually. Should be two files like:
+  # ./.mozilla/native-messaging-hosts/tridactyl.json
+  # ./.local/share/tridactyl
+  myfirefox = firefox-devedition.override {
+    extraNativeMessagingHosts = [
+      pkgs.tridactyl-native
+    ];
+  };
+in [
+  kitty
+  # Desktop xfce things
+  xfce.xfce4-panel
+  xfce.xfce4-i3-workspaces-plugin
+  xfce.xfce4-screensaver
+  nitrogen
+  # GUI Apps
+  myfirefox
+  chromium
+  slack
+  spotify
+  discord
+  # Signal complains when it's out of date. Need to use unstable.
+  unstable.signal-desktop
+  # Sound
+  qjackctl
+  playerctl
+  pavucontrol
+  # Misc
+  remmina
+  (pass.withExtensions (ext: with ext; [pass-otp]))
+  xdotool
+  xsel
+]
