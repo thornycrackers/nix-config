cae907a9c89b94d037f644986b73e7682cf46383
Cody Hiar <cody@hiar.ca>
2024-06-03 16:36:29 -0600

    Linting

--- src/nvim/lua/utils.lua
+++ src/nvim/lua/utils.lua
@@ -12,22 +12,27 @@ function markdown_link_wrap()
     selected_text = current_line:sub(start_col, end_col)
 
     -- Replace the text
-    vim.api.nvim_buf_set_text(0, current_line_number-1, start_col - 1, current_line_number -1, end_col, {'[' .. selected_text .. '][]'})
+    vim.api.nvim_buf_set_text(0, current_line_number - 1, start_col - 1,
+                              current_line_number - 1, end_col,
+                              {'[' .. selected_text .. '][]'})
     -- Set mouse in brackets and enter insert mode
     vim.api.nvim_win_set_cursor(0, {current_line_number, end_col + 3})
     -- Send the '<Esc>' key to exit visual mode
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc>', true, false, true), 'n', true)
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc>', true, false,
+                                                         true), 'n', true)
     vim.api.nvim_command('startinsert')
 end
 
 -- Quick command for incrementing urls in markdown
 function yank_replace_url()
     vim.api.nvim_feedkeys('yyP', "n", true)
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-a>", true, true, true), "n", true)
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-a>", true, true,
+                                                         true), "n", true)
     vim.api.nvim_feedkeys('WC', "n", true)
 end
 
-vim.api.nvim_set_keymap('v', '<leader>em', '<cmd>lua markdown_link_wrap()<cr>', { noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>el', '<cmd>lua yank_replace_url()<cr>', { noremap = true, silent = true})
-
+vim.api.nvim_set_keymap('v', '<leader>em', '<cmd>lua markdown_link_wrap()<cr>',
+                        {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>el', '<cmd>lua yank_replace_url()<cr>',
+                        {noremap = true, silent = true})
 
