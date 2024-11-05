4ce5c9dca333679928661d83f6f3e444f8ea6bf6
Cody Hiar <cody@hiar.ca>
2024-11-05 10:21:27 -0700

    Add a centos container

---
+++ playground/os-provisioning/setup_vm_centos_9_stream.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+# vim: set filetype=sh
+#
+# Author: Cody Hiar
+# Date: 2024-10-28
+#
+# Description: Script to setup incus container
+#
+# Set options:
+#   e: Stop script if command fails
+#   u: Stop script if unset variable is referenced
+#   x: Debug, print commands as they are executed
+#   o pipefail:  If any command in a pipeline fails it all fails
+#
+# IFS: Internal Field Separator
+set -xeuo pipefail
+IFS=$'\n\t'
+
+container_name="centos9"
+container_exists=$(incus ls | grep "$container_name" || true)
+
+# Only create the container if it doesn't already exist
+if [[ -z "$container_exists" ]]; then
+    incus launch images:centos/9-Stream "$container_name"
+fi
+
+# Sometimes there are resolution failures, sleep for a couple seconds
+sleep 2
+
+# Install/Enable ssh server for ansible
+incus exec "$container_name" -- yum update -y
+incus exec "$container_name" -- yum install -y openssh-server
+incus exec "$container_name" -- systemctl enable sshd
+incus exec "$container_name" -- systemctl start sshd
+
+# Copy key into machine and set permissions correctly for root user
+incus exec "$container_name" -- mkdir -p /root/.ssh
+incus exec "$container_name" -- chmod 700 /root/.ssh
+incus file push ~/.ssh/id_rsa.pub "$container_name"/root/.ssh/authorized_keys
+incus exec "$container_name" -- chmod 600 /root/.ssh/authorized_keys
+incus exec "$container_name" -- chown root:root /root/.ssh/authorized_keys
+
+# Create the ansible user
+incus exec "$container_name" -- adduser ansible
+incus exec "$container_name" -- usermod -aG wheel ansible
+incus exec "$container_name" -- /bin/bash -c 'echo "%wheel ALL=(ALL) NOPASSWD:ALL" >>/etc/sudoers'
+
+# Copy key into machine and set permissions correctly for ansible user
+incus exec "$container_name" -- mkdir -p /home/ansible/.ssh
+incus exec "$container_name" -- chmod 700 /home/ansible/.ssh
+incus file push ~/.ssh/id_rsa.pub "$container_name"/home/ansible/.ssh/authorized_keys
+incus exec "$container_name" -- chmod 600 /home/ansible/.ssh/authorized_keys
+incus exec "$container_name" -- chown -R ansible:ansible /home/ansible/.ssh
+
+# Generate the inventory file for ansible
+# The IP changes every time we create a new container so I automate the
+# creation of the inventory file.
+ansible_ip=$(incus info "$container_name" | grep "10.0" | awk '{ print $2 }' | cut -d'/' -f1)
+echo "$ansible_ip"
+cat <<EOF >inventory
+[myhosts]
+myserver ansible_host=$ansible_ip ansible_user=ansible
+EOF
