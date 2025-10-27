ffd4f42c6b2031146a1bc66b7000df7ef21da57d
Cody Hiar <cody@hiar.ca>
2025-10-27 14:00:16 -0600

    Uninstall coreutils to fix error

--- hosts/shared/packages-darwin.nix
+++ hosts/shared/packages-darwin.nix
@@ -1,6 +1,9 @@
 pkgs: with pkgs; [
   # The core utils that ship with mac are old
-  coreutils
+  # NOTE: when I install coreutils it will cause:
+  # stty: 'standard input': unable to perform all requested operations
+  # When kitty boots up
+  # coreutils
   watch
   docker
   colima
