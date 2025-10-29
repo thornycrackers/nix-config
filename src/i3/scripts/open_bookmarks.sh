72535049e375b5c704db4b1d3e4d9f7263e33851
Cody Hiar <cody@hiar.ca>
2025-10-29 11:09:25 -0600

    Updating open_bookmarks to allow multiple selections

--- src/i3/scripts/open_bookmarks.sh
+++ src/i3/scripts/open_bookmarks.sh
@@ -27,10 +27,38 @@ IFS=$'\n\t'
 # get rofi to return the index of the selection you've chosen so I used this as
 # an alternative.
 items=$(cat -n ~/.bookmarks.csv)
-selection=$(echo "$items" | awk -F ',' '{print $1}' | rofi -dmenu -i -P Bookmark)
-# Make sure there was a selection and I didn't cancel out of the window
-if [[ -n "$selection" ]]; then
-    index=$(echo "$selection" | awk '{ print $1 }')
-    url=$(awk "NR==$index" ~/.bookmarks.csv | cut -d ',' -f 2)
-    xdg-open "$url"
-fi
+
+# Loop to allow multiple selections with shift+enter
+while true; do
+    selections=$(echo "$items" | awk -F ',' '{print $1}' | rofi -dmenu -multi-select -i -P Bookmark \
+        -theme-str 'listview { columns: 1; } element { padding: 2px 8px; } element selected { background-color: @selected-normal-background; } element-text { highlight: bold; }' \
+        -mesg "Ctrl+Space: Select | Enter: Open Selected & Close | Shift+Enter: Open Selected & Keep Menu")
+    exit_code=$?
+
+    # Check if cancelled
+    if [[ $exit_code -eq 1 ]]; then
+        break
+    fi
+
+    # Process all selected items
+    if [[ -n "$selections" ]]; then
+        while IFS= read -r line; do
+            if [[ -n "$line" ]]; then
+                index=$(echo "$line" | awk '{ print $1 }')
+                url=$(awk "NR==$index" ~/.bookmarks.csv | cut -d ',' -f 2)
+                nohup xdg-open "$url" >/dev/null 2>&1 &
+            fi
+        done <<<"$selections"
+
+        # If shift+enter was used (exit code 10), continue the loop to keep menu open
+        if [[ $exit_code -eq 10 ]]; then
+            continue
+        else
+            # Normal enter (exit code 0), close the menu
+            break
+        fi
+    else
+        # No selection made, break
+        break
+    fi
+done
