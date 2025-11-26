0ec8e035ffaf0a57401c80794f6afc855c1ce081
Cody Hiar <cody@hiar.ca>
2025-11-26 13:55:28 -0700

    Updated hmts.nvim version to be compatible with nvim and stop error messages

--- src/nvim/nix/vim-hmts.nix
+++ src/nvim/nix/vim-hmts.nix
@@ -1,12 +1,12 @@
 { vimUtils, fetchFromGitHub }:
 vimUtils.buildVimPlugin rec {
   pname = "hmts.nvim";
-  version = "1.2.2";
+  version = "1.3.0";
   src = fetchFromGitHub {
     owner = "calops";
     repo = "${pname}";
     rev = "v${version}";
-    sha256 = "sha256-jUuztOqNBltC3axa7s3CPJz9Cmukfwkf846+Z/gAxCU=";
+    sha256 = "sha256-j/RFJgCbaH+V2K20RrQbsz0bzpN8Z6YAKzZMABYg/OU=";
   };
   meta.homepage = "https://github.com/calops/hmts.nvim";
 }
