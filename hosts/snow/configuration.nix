a2838b07d2736c06368e0803cd8539fc8914db03
Cody Hiar <cody@hiar.ca>
2026-04-09 10:07:50 -0600

    Update lidSwitch -> HandleLidSwitch from build warning

--- hosts/snow/configuration.nix
+++ hosts/snow/configuration.nix
@@ -230,8 +230,8 @@
   systemd.targets.hibernate.enable = false;
   systemd.targets.hybrid-sleep.enable = false;
   services.logind = {
-    lidSwitch = "ignore";
     settings.Login = {
+      HandleLidSwitch = "ignore";
       IdleAction = "ignore";
       IdleActionSec = 0;
     };
