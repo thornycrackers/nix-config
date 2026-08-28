f4e451bebc2df002b6e466fd5cffd31cac3cc84a
Cody Hiar <cody@hiar.ca>
2026-08-28 09:57:38 -0600

    Update location of nvim-obsidian to thornycrackers-forks

--- src/nvim/nix/obsidian-nvim.nix
+++ src/nvim/nix/obsidian-nvim.nix
@@ -3,11 +3,11 @@ vimUtils.buildVimPlugin {
   pname = "obsidian-nvim";
   version = "2026-04-09";
   src = fetchFromGitHub {
-    owner = "thornycrackers";
+    owner = "thornycrackers-forks";
     repo = "obsidian.nvim";
     rev = "main";
     sha256 = "sha256-ZWbkqW+nGS6PY7ycaU5S/XK7XZJU6tgSetukxcRUA8M=";
   };
   nvimRequireCheck = "obsidian";
-  meta.homepage = "https://github.com/thornycrackers/obsidian.nvim";
+  meta.homepage = "https://github.com/thornycrackers-forks/obsidian.nvim";
 }
